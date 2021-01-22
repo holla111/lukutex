a1f2c01f8b1f78c52a0d7e3c9d198918f233578c
Lê Thành Đạt <thanhdat@Les-MacBook-Pro.local>
2021-01-22 19:47:42 +0700

    additional logic for marketsCpn

--- src/mobile/components/Markets/index.tsx
+++ src/mobile/components/Markets/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useSelector } from 'react-redux';
 import {Table} from '../';
 import {TabPanel} from '../../../components';
-import { selectMarkets } from '../../../modules/public/markets';
+import { Market, selectMarkets } from '../../../modules/public/markets';
 
 export interface MarketsProps {
     /**
@@ -15,36 +15,47 @@ const MarketsComponent : React.FC<MarketsProps> = ({headers}) => {
     const markets = useSelector(selectMarkets);
 
     const [currentTabIndex, setCurrentTabIndex] = React.useState(0);
+    const [currentMarketsFilter, setCurrentMarketsFilter] = React.useState<Market[]>(markets);
+
+    let currentBidUnitsList: string[] = [''];
+
+    React.useEffect(() => {
+        const marketsFilter = currentTabIndex === 0 ? markets : markets.filter(market => market.quote_unit === currentBidUnitsList[currentTabIndex]);
+        setCurrentMarketsFilter(marketsFilter);
+    },[currentTabIndex,markets]);
+
     const defaultHeader = ['Name','Current', 'Chg'];
 
-    const renderTabs = () => [
-        {
-            content: currentTabIndex === 0 ? renderTab() : null,
-            label: 'Rising List',
-        },
-        {
-            content: currentTabIndex === 1 ? renderTab() : null,
-            label: 'Decline List',
-        },
-        {
-            content: currentTabIndex === 2 ? renderTab() : null,
-            label: '24h Volume',
-        },
-    ];
+    const formatFilteredMarkets = (list: string[], market: Market) => {
+        if (!list.includes(market.quote_unit)) {
+            list.push(market.quote_unit);
+        }
+
+        return list;
+    };
+
+    if (markets.length > 0) {
+        currentBidUnitsList = markets.reduce(formatFilteredMarkets, currentBidUnitsList);
+    }
+
+    const renderTabs = () => currentBidUnitsList.map((name,i) => ({
+        content: currentTabIndex === i ? renderTab() : null,
+        label: name ? name.toUpperCase() : 'ALL',
+    }));
 
     const renderTab = () => {
         return (
-            <Table headers={headers ? headers : defaultHeader} markets={markets}/>
+            <Table headers={headers ? headers : defaultHeader} markets={currentMarketsFilter}/>
         );
     };
 
     return (
         <div className="cr-mobile-market">
-            <TabPanel
+            {markets ? <TabPanel
                 panels={renderTabs()}
                 currentTabIndex={currentTabIndex}
                 onCurrentTabChange={setCurrentTabIndex}
-            />
+            /> : null}
         </div>
     );
 };
