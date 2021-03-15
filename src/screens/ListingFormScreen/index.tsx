9743fa3ac5c502eb078be9d70fd44b94acab2207
nhatduy51 <60657954+nhatduy51@users.noreply.github.com>
2021-03-16 01:22:47 +0700

    duy-listingform

--- src/screens/ListingFormScreen/index.tsx
+++ src/screens/ListingFormScreen/index.tsx
@@ -1,48 +1,88 @@
 import * as React  from "react";
 import { useForm } from "react-hook-form";
-import {Form, Col,} from 'react-bootstrap';
+import {Form, Col, Button} from 'react-bootstrap';
 
 
 interface IFormInput {
   firstName: string;
   lastName: string;
-  age: number;
+  email: string;
+  nameOfProject: string;
+  contact: string;
+  projectWebsite: string;
+  description: string;
+  media: string;
+
 }
 export const ListingFormScreen: React.FC = () => {
 
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const [formValue, setFormValue] = React.useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    nameofproject: '',
+    contact: '',
+    projectwebsite: '',
+    description: '',
+    media: '',
+  });
 
-  const onSubmit = (data: IFormInput) => console.log(data);
-
-  
+  const { register, handleSubmit, errors } = useForm<IFormInput>();
   type RBRef = (string & ((ref: Element | null) => void));
+
+
+  const handleSubmitForm = (data: IFormInput) => (
+    console.log(data)
+  );
+
+  const handleValidInput = (e: any) => (
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value
+    })
+  )
 
   const renderForm = () => {
     return (
       <div className="listingformscreen">
         <div className="listing__token">
           <div className="listing__info">
-          <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form onSubmit={handleSubmit(handleSubmitForm)}>
             <Form.Row>
               <Form.Group as={Col} controlId="formGridEmail">
                 <Form.Label>FirstName</Form.Label>
                 <Form.Control type="text" placeholder="+" 
+                      name="firstname"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef} 
                 />
+                {errors.firstName && errors.firstName.type === "required" && (
+                  <div className="error">You must enter your name.</div>
+                )}
               </Form.Group>
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>LastName</Form.Label>
                 <Form.Control type="text" placeholder="+"
+                      name="lastname"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef}
                 />
+                {errors.lastName && errors.lastName.type === "required" && (
+                  <div className="error">You must enter your name.</div>
+                )}
               </Form.Group>
             </Form.Row>
             <Form.Row>
               <Form.Group as={Col} controlId="formGridEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="text" placeholder="+"
+                      name="email"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef}
                 />
+                {errors.email && errors.email.type === "required" && (
+                  <div className="error">You must enter your name.</div>
+                )}
               </Form.Group>
             </Form.Row>
           </Form>
@@ -53,34 +93,45 @@ export const ListingFormScreen: React.FC = () => {
               <Form.Group>
                 <Form.Label>Name of Project</Form.Label>
                 <Form.Control type="text" placeholder="+" 
+                      name="nameOfProject"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef}
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>CEO Contact(telegram, email...)</Form.Label>
                 <Form.Control type="text" placeholder="+"
+                      name="contact"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef}
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Project Website</Form.Label>
                 <Form.Control type="url" placeholder="+" 
+                      name="projectWebsite"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef}
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Description</Form.Label>
                 <Form.Control type="text" placeholder="+"
+                      name="description"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef}
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Media Link(FaceBook, Telegram Group...)</Form.Label>
                 <Form.Control type="url" placeholder="+"
+                      name="media"
+                      onChange={handleValidInput}
                       ref={register({ required: true}) as RBRef} 
                 />
               </Form.Group>
             </Col>
+            <Button type="submit">Submit form</Button>
           </Form>
           </div>
         </div>
