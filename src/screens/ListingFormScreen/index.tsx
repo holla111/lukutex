b1d58885b204337b4e43da3f4a4e38149e889dfc
nhatduy51 <60657954+nhatduy51@users.noreply.github.com>
2021-03-18 01:00:00 +0700

    duy-listingform

--- src/screens/ListingFormScreen/index.tsx
+++ src/screens/ListingFormScreen/index.tsx
@@ -49,8 +49,13 @@ export const ListingFormScreen: React.FC = () => {
     console.log(formValue)
     return (
       <form className="listingformscreen" onSubmit={handleSubmit(onSubmit)}>
+        {/*  */}
         <div className="listing__token">
           <div className="listing__info">
+            <div className="listing__info-heading">
+              <h3 className="heading-title">Listen Token</h3>
+              <div className="heading-line"></div>
+            </div>
             <div className="listing__info-name">
               <div className="listing__info-name-firstname">
                 <label className="Input-label">FirstName</label>
@@ -69,7 +74,6 @@ export const ListingFormScreen: React.FC = () => {
                       type='text'
                       ref={register({ required: true})}
                       onChange={handleValidInput}
-                      style={{marginLeft: 10}}
                 />
                 {errors.lastname && <p className='error'>This field is required</p>}
               </div>
@@ -86,6 +90,10 @@ export const ListingFormScreen: React.FC = () => {
             </div>
           </div>
           <div className="project__info">
+            <div className="project__info-heading">
+              <h3 className="heading-title">Project Infomation</h3>
+              <div className="heading-line"></div>
+            </div>
             <div className="project__info-wrap">
               <label className="Input-label">Name of Project</label>
               <input className="Input-text"
@@ -140,6 +148,10 @@ export const ListingFormScreen: React.FC = () => {
         {/*  */}
         <div className="token__info">
           <div className="token__info-infomation">
+            <div className="token__info-infomation-heading">
+              <h3 className="heading-title">Token Infomation</h3>
+              <div className="heading-line"></div>
+            </div>
             <div className="token__info-infomation-lock-1">
               <div className="token__info-coin">
                 <div className="token__info-coi-wrap">
@@ -165,7 +177,47 @@ export const ListingFormScreen: React.FC = () => {
                   {errors.media && <p className='error'>This field is required</p>}
                 </div>
               </div>
+            </div>
+            <div className="token__info-infomation-lock-2">
+              <div className="token__info-explorer">
+                <div className="token__info-explorer__texterea">
+                  <div className="project__info-wrap">
+                    <label className="Input-label">Explorer Link</label>
+                    <textarea className="Input-text"
+                            name='explorerlink'
+                            ref={register({ required: true})}
+                            onChange={handleValidInput}
+                    />
+                    {errors.description && <p className='error'>This field is required</p>}
+                  </div>
+                </div>
+                <div className="token__info-explorer__supply-logo">
+                  <div className="project__info-wrap">
+                    <label className="Input-label">Total Supply</label>   
+                    <input className="Input-text"
+                            name='totalsupply'
+                            ref={register({ required: true})}
+                            onChange={handleValidInput}
+                    />
+                    {errors.projectwebsite && <p className='error'>This field is required</p>}
+                  </div>
+                  <div className="project__info-wrap">
+                    <label className="Input-label">Logo ( 128x128) </label>   
+                    <input className="Input-text"
+                            style={{color: 'rgba(0, 0, 0, 0)'}}              
+                            name='logo'
+                            type='file'
+                            ref={register({ required: true})}
+                            onChange={handleValidInput}
+                    />
+                    {errors.projectwebsite && <p className='error'>This field is required</p>}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="token__info-infomation-lock-3">
               <div className="token__info-cointype">
+                <label className="Input-label">Coin Type</label>   
                 <Radio.Group >
                   <Radio style={radioStyle} value={1}>
                     Ethereum (Fee: 100 USDT)
@@ -182,43 +234,18 @@ export const ListingFormScreen: React.FC = () => {
                 </Radio.Group>
               </div>
             </div>
-            <div className="token__info-infomation-lock-2">
-              <div className="token__info-explorer">
-                <div className="project__info-wrap">
-                  <label className="Input-label">Explorer Link</label>
-                  <textarea className="Input-text"
-                          name='explorerlink'
-                          ref={register({ required: true})}
-                          onChange={handleValidInput}
-                  />
-                  {errors.description && <p className='error'>This field is required</p>}
-                </div>
-                <div className="project__info-wrap">
-                  <label className="Input-label">Total Supply</label>   
-                  <input className="Input-text"
-                          name='totalsupply'
-                          ref={register({ required: true})}
-                          onChange={handleValidInput}
-                  />
-                  {errors.projectwebsite && <p className='error'>This field is required</p>}
-                </div>
-                <div className="project__info-wrap">
-                  <label className="Input-label">Logo ( 128x128) </label>   
-                  <input className="Input-text"
-                          name='logo'
-                          type='file'
-                          ref={register({ required: true})}
-                          onChange={handleValidInput}
-                  />
-                  {errors.projectwebsite && <p className='error'>This field is required</p>}
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         {/*  */}
         <div className="listing__plan">
+          <div className="listing__plan-heading">
+              <h3 className="heading-title">Listing Plan</h3>
+              <div className="heading-line"></div>
+          </div>
           <div className="listing__plan-openpair">
+            <label className="listing__plan-openpair-pairopen">
+              What pair you want open? 
+            </label>
             <Radio.Group >
               <Radio style={radioStyle} value={1}>
                 Ethereum (Fee: 100 USDT)
@@ -234,20 +261,24 @@ export const ListingFormScreen: React.FC = () => {
               </Radio>
             </Radio.Group>
           </div>
-          <div className="listing__Plan-date">
-            <Space direction="vertical" size={12}>
-              <DatePicker renderExtraFooter={() => 'extra footer'} />
-            </Space>
-          </div>
-          <div className="listing__trade-date">
-            <Space direction="vertical" size={12}>
-              <DatePicker renderExtraFooter={() => 'extra footer'} />
-            </Space>
+          <div className="listing__plan-date">
+            <div className="listing__Plan-listingdate">
+              <Space direction="vertical" size={12}>
+                <DatePicker renderExtraFooter={() => 'extra footer'} />
+              </Space>
+            </div>
+            <div className="listing__plan-tradedate">
+              <Space direction="vertical" size={12}>
+                <DatePicker renderExtraFooter={() => 'extra footer'} />
+              </Space>
+            </div>
           </div>
         </div>
+        {/*  */}
         <Button htmlType="submit" color="primary">
             submit
         </Button>
+        {/*  */}
       </form>
     );
   }
