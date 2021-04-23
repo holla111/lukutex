a3bcd9ec2b6d354ff0986787c5d4fb91aa1494f4
Phuoc Huynh <huynhphvan@gmail.com>
2021-04-23 20:26:15 +0700

    change api

--- src/modules/public/currencies/sagas/currenciesFetchSaga.ts
+++ src/modules/public/currencies/sagas/currenciesFetchSaga.ts
@@ -1,6 +1,6 @@
 import { call, put, takeLeading } from 'redux-saga/effects';
 import { API, RequestOptions } from '../../../../api';
-// import pluginAPI from '../../../../plugins/api';
+import pluginAPI from '../../../../plugins/api';
 import { alertPush } from '../../alert';
 import {
     currenciesData,
@@ -16,25 +16,24 @@ export function* rootCurrenciesSaga() {
     yield takeLeading(CURRENCIES_FETCH, currenciesFetchSaga);
 }
 
-// interface ParentCurrency {
-//     id: string;
-//     name: string;
-//     blockchain_key: string;
-// }
+interface ParentCurrency {
+    id: string;
+    blockchain_key: string;
+}
 
 export function* currenciesFetchSaga() {
     try {
         const currencies = yield call(API.get(currenciesOptions), '/public/currencies');
-        // const parent_currencies = yield pluginAPI.get<ParentCurrency[]>('/currencies/fetch/all');
-        // const currencies_with_blockchainkey = [...currencies].map(currency => {
-        //     const blockchain_key = parent_currencies.find(cur => cur.id === currency.id);
-        //     console.log(blockchain_key);
-            
-
-        // });
-        // console.log(currencies_with_blockchainkey, currencies);
+        const parent_currencies = yield pluginAPI.get<ParentCurrency[]>('wallet/parent-currencies/fetch/all');
         
-        yield put(currenciesData(currencies));
+        const currencies_with_blockchainkey = [...currencies].map(currency => {
+            const cur = parent_currencies.data.find(cur => cur.id === currency.id);
+            return {
+                ...currency,
+                blockchain_key: cur ? cur.blockchain_key : null
+            }
+        });
+        yield put(currenciesData(currencies_with_blockchainkey));
     } catch (error) {
         yield put(currenciesError());
         yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
