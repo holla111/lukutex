7aa3d75bfa51056b1cf0b591f7c9258f74b43040
Phuoc Huynh <huynhphvan@gmail.com>
2021-05-05 10:18:01 +0700

    fix ui

--- src/plugins/Wallets/containers/WithdrawInfo/index.tsx
+++ src/plugins/Wallets/containers/WithdrawInfo/index.tsx
@@ -6,6 +6,27 @@ import Select from 'react-select';
 import { selectAllChildCurrencies, selectCurrencies, Wallet } from '../../../../modules';
 import { CurrencyInfo } from '../../components/CurrencyInfo';
 import { TradeList } from '../../components/TradeList';
+import styled from 'styled-components';
+
+const DepositButtonStyles = styled.h2`
+    color: #fff;
+    padding: 0.5rem 0;
+    font-size: 2rem;
+    outline: none;
+    border: none;
+    cursor: pointer;
+`;
+
+const WithdrawButtonStyles = styled.h2`
+    color: #fff;
+    padding: 0.5rem 0;
+    font-size: 2rem;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    margin-left: 1rem;
+    border-bottom: 4px solid #2a9d8f;
+`;
 
 const SelectStyles = {
     option: (provided, state) => ({
@@ -87,11 +108,11 @@ export const WithdrawInfo: React.FC<WithdrawInfoProps> = (props: WithdrawInfoPro
     };
 
     return (
-        <div className="container" style={{ padding: '50px 0' }}>
+        <div className="container" style={{ padding: '20px 0' }}>
             <div className="row">
-                <div className="col-8">
-                    <span style={{ fontSize: '3rem', color: '#3c78e0' }}>Withdraw/ </span>
-                    <span className="text-secondary" style={{ fontSize: '2rem', cursor: 'pointer' }} onClick={() => history.push({ pathname: `/new-wallets/deposit/${currency_id.toUpperCase()}` })}>Deposit</span>
+                <div className="col-8 d-flex flex-row">
+                    <DepositButtonStyles onClick={() => history.push({ pathname: `/new-wallets/deposit/${currency_id.toUpperCase()}` })}>Deposit</DepositButtonStyles>
+                    <WithdrawButtonStyles>Withdraw</WithdrawButtonStyles>
                 </div>
                 <div className="col-4">
                     <Select
@@ -113,12 +134,12 @@ export const WithdrawInfo: React.FC<WithdrawInfoProps> = (props: WithdrawInfoPro
                         <svg style={{ width: '20px' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4.791a.723.723 0 00.716-.729V2.729c0-.402-.32-.729-.716-.729a.723.723 0 00-.716.73v1.332c0 .402.32.73.716.73zM6.884 6.51a.713.713 0 01-.716.72.733.733 0 01-.508-.2l-.936-.94a.713.713 0 01-.212-.515c0-.197.076-.385.212-.515a.734.734 0 011.016 0l.932.934c.136.13.212.319.212.516zm4.436 14.032h1.336c.396 0 .716.326.716.729 0 .402-.32.729-.716.729h-1.332a.723.723 0 01-.716-.73c0-.38.32-.707.712-.729zM2.716 10.268h1.332c.388 0 .716.335.716.73 0 .401-.32.728-.716.728H2.716A.723.723 0 012 10.998c0-.394.328-.73.716-.73zm16.776-4.694a.696.696 0 00-.212-.511.701.701 0 00-1.02 0l-.932.934a.713.713 0 00-.212.516c0 .197.076.386.212.515.14.135.324.202.508.202a.719.719 0 00.508-.206l.932-.934a.73.73 0 00.216-.516zm.46 4.694h1.332c.388 0 .716.335.716.73 0 .401-.32.728-.716.728h-1.332a.723.723 0 01-.716-.729c0-.402.32-.73.716-.73zm-5.964 8.294h-3.976a.723.723 0 00-.716.73c0 .402.32.729.716.729h3.976a.723.723 0 00.716-.73c0-.402-.32-.729-.716-.729zM12 5.981c1.612 0 3.124.625 4.26 1.76A5.984 5.984 0 0118.024 12c0 1.61-.628 3.122-1.764 4.258a5.982 5.982 0 01-4.26 1.76 5.982 5.982 0 01-4.26-1.76A5.984 5.984 0 015.976 12c0-1.61.628-3.123 1.764-4.258A5.982 5.982 0 0112 5.982z" fill="currentColor"></path>
                         </svg>
-                        <span className="ml-2">{intl.formatMessage({id: 'page.body.plugins.wallet.withdraw.info.tips'})}: </span>
+                        <span className="ml-2">{intl.formatMessage({ id: 'page.body.plugins.wallet.withdraw.info.tips' })}: </span>
                     </div>
                     <div className="ml-2 mt-2">
-                        <p>{intl.formatMessage({id: 'page.body.plugins.wallet.withdraw.info.tip1'})}</p>
-                        <p>{intl.formatMessage({id: 'page.body.plugins.wallet.withdraw.info.tip2'})}</p>
-                        <p>{intl.formatMessage({id: 'page.body.plugins.wallet.withdraw.info.tip3'})}</p>
+                        <p>{intl.formatMessage({ id: 'page.body.plugins.wallet.withdraw.info.tip1' })}</p>
+                        <p>{intl.formatMessage({ id: 'page.body.plugins.wallet.withdraw.info.tip2' })}</p>
+                        <p>{intl.formatMessage({ id: 'page.body.plugins.wallet.withdraw.info.tip3' })}</p>
                     </div>
                 </div>
             </div>
